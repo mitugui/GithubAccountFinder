5302b793bafc777aef9af27199902b80cb15656c
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-06-06 11:14:36 -0300

    feat: add routing system with createBrowserRouter, RouterProvider, and Outlet

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// Pages
+import Home from "./routes/Home"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      }
+    ]
+  }
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
