cbf65f1c64db5510ad8cb810147cfd87524d2e54
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-06-06 20:47:44 -0300

    feat: add Search component
    Adds the Search component for searching GitHub users.

---
+++ src/interfaces/iUser.ts
@@ -0,0 +1,7 @@
+export interface iUser {
+    avatar_url: string,
+    login: string,
+    location: string,
+    followers: number,
+    following: number
+}
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+import Search from "../../components/Search"
+import { iUser } from "../../interfaces/iUser"
+
 const Home = () => {
+    const [user, setUser] = useState<iUser | null>(null)
+
+    const loadUser = async (userName: string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json()
+
+        const { avatar_url, login, location, followers, following } = data
+
+        const userData: iUser = {
+            avatar_url,
+            login,
+            location,
+            followers,
+            following
+        }
+
+        setUser(userData)
+    }
+
     return (
-        <div>Home</div>
+        <div>
+            <Search loadUser={loadUser} />
+            {user && <p>{user.login}</p>}
+        </div>
     )
 }
 
