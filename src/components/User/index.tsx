c81025492d5f7b2905d22cebbe082cefd53cb2eb
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-06-07 13:59:55 -0300

    style: add styling to User component

--- src/components/User/index.tsx
+++ src/components/User/index.tsx
@@ -1,6 +1,8 @@
 import { iUser } from "../../interfaces/iUser"
 import { MdLocationPin } from "react-icons/md"
 
+import classes from "./User.module.css"
+
 const User = ({
     avatar_url,
     login,
@@ -9,23 +11,23 @@ const User = ({
     following
 }: iUser) => {
     return (
-        <div>
+        <div className={classes.user}>
             <img src={avatar_url} alt={`Imagem de avatar de ${login}`}></img>
             <h2>{login}</h2>
             {location && (
-                <p>
+                <p className={classes.location}>
                     <MdLocationPin />
                     <span>{location}</span>
                 </p>
             )}
-            <div>
+            <div className={classes.stats}>
                 <div>
-                    <p>Seguidores:</p>
-                    <p>{followers}</p>
+                    <p>Seguidores</p>
+                    <p className={classes.number}>{followers}</p>
                 </div>
                 <div>
-                    <p>Seguindo:</p>
-                    <p>{following}</p>
+                    <p>Seguindo</p>
+                    <p className={classes.number}>{following}</p>
                 </div>
             </div>
         </div>
