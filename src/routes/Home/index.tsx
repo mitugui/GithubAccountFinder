afe91f173efe9b2ed8835fb8da3d7bcef501afef
Cristian Oliveira Mitugui <cristian.mitugui04@gmail.com>
2024-06-07 13:22:54 -0300

    feat: add Error component for handling user not found errors

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,15 +1,25 @@
 import { useState } from "react"
 import Search from "../../components/Search"
 import User from "../../components/User"
+import Error from "../../components/Error"
 import { iUser } from "../../interfaces/iUser"
 
 const Home = () => {
     const [user, setUser] = useState<iUser | null>(null)
+    const [error, setError] = useState(false)
 
     const loadUser = async (userName: string) => {
         const res = await fetch(`https://api.github.com/users/${userName}`)
 
         const data = await res.json()
+
+        setError(false)
+        setUser(null)
+
+        if (res.status === 404) {
+            setError(true)
+            return
+        }
 
         const { avatar_url, login, location, followers, following } = data
 
@@ -28,6 +38,7 @@ const Home = () => {
         <div>
             <Search loadUser={loadUser} />
             {user && <User {...user} />}
+            {error && <Error />}
         </div>
     )
 }
